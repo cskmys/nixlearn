77f1270095efd70dd556cd2cb0b47005e56c2015
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-04-12 10:52:17 +0200

    lfd107x ch15 shells, bash, command line

---
+++ lfd107x/rsrcs/lab/l15_02.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+mk_tmp_dir(){
+  TMP_DIR_NAME=$(basename "${0}" | awk -F"." '{print $1}')
+  TMP_DIR_PATH=$(mktemp -d "/tmp/${TMP_DIR_NAME}.XXX")
+  cd "${TMP_DIR_PATH}" || { echo "can't mk_tmp_dir"; exit 1;}
+}
+rm_tmp_dir(){
+  cd "/tmp" || { echo "can't go to /tmp"; exit 1;}
+  rm -r "${TMP_DIR_PATH}"
+}
+setup(){
+  mk_tmp_dir
+}
+cleanup(){
+  rm_tmp_dir
+}
+cleanup_w_permission(){
+  echo "You want to cleanup[*/n]?"
+  read -r opt
+  case "${opt}" in
+    "no" | "NO" | "No" | "nO" | "n" | "N") echo "not cleaning up";;
+    *) cleanup;;
+  esac
+}
+exit_func(){
+  cleanup
+  [[ -z "${1}" ]] && exit "0" || exit "${1}"
+}
+is_pgm_installed(){
+  if which "${1}"; then
+    return 0
+  fi
+  return 1
+}
+setup_pgm(){
+  pgm="${1}"
+  if [[ ((${#} -eq 1)) ]]; then
+    pkg="${1}"
+  elif [[ ((${#} -eq 2)) ]]; then
+    pkg="${2}"
+  else
+    echo "provide just binary name if binary name and package name are same or both"
+  fi
+
+  if is_pgm_installed "${pgm}" "${pkg}"; then
+    echo "${pgm} from ${pkg} is installed"
+    return 0
+  fi
+  echo "${pkg} package needs to be installed for command ${pgm}"
+  sudo apt -y install "${pkg}"
+}
+lab(){
+  echo "ls"
+  ls "/etc/passwd" "/etc/passwd_not"
+  echo ""
+  ls "/etc/passwd" "/etc/passwd_not" > "./sout" 2> "./serr"
+  ls "/etc/passwd" "/etc/passwd_not" > "./sboth" 2>&1
+  ls "/etc/passwd" "/etc/passwd_not" > "./sout_only" 2> "/dev/null"
+  ls "/etc/passwd" "/etc/passwd_not" 2> "./serr_only" | sort
+  echo "diff stdout"
+  diff "./sout" "./sout_only"
+  echo ""
+  echo "diff stderr"
+  diff "./serr" "./serr_only"
+  echo ""
+  echo "sboth"
+  cat "./sboth"
+}
+
+setup
+lab "${@}"
+ret="${?}"
+cleanup
+exit "${ret}"
