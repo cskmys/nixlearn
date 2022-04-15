f860963845d8b3c731797b96799d7c43afc691fa
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-04-15 05:35:11 +0200

    lfd108x ch04 networking

---
+++ lfd108x/rsrcs/lab/l04_01.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+mk_tmp_dir(){
+  TMP_DIR_NAME=$(basename "${0}" | awk -F"." '{print $1}')
+  TMP_DIR_PATH=$(mktemp -d "/tmp/${TMP_DIR_NAME}.XXX")
+  cd "${TMP_DIR_PATH}" || { echo "can't mk_tmp_dir"; exit 1;}
+}
+rm_tmp_dir(){
+  cd "/tmp" || { echo "can't go to /tmp"; exit 1;}
+  rm -r "${TMP_DIR_PATH}"
+}
+setup(){
+  mk_tmp_dir
+}
+cleanup(){
+  rm_tmp_dir
+}
+cleanup_w_permission(){
+  echo "You want to cleanup[*/n]?"
+  read -r opt
+  case "${opt}" in
+    "no" | "NO" | "No" | "nO" | "n" | "N") echo "not cleaning up";;
+    *) cleanup;;
+  esac
+}
+exit_func(){
+  cleanup
+  [[ -z "${1}" ]] && exit "0" || exit "${1}"
+}
+is_pgm_installed(){
+  if which "${1}"; then
+    return 0
+  fi
+  return 1
+}
+setup_pgm(){
+  pgm="${1}"
+  if [[ ((${#} -eq 1)) ]]; then
+    pkg="${1}"
+  elif [[ ((${#} -eq 2)) ]]; then
+    pkg="${2}"
+  else
+    echo "provide just binary name if binary name and package name are same or both"
+  fi
+
+  if is_pgm_installed "${pgm}" "${pkg}"; then
+    echo "${pgm} from ${pkg} is installed"
+    return 0
+  fi
+  echo "${pkg} package needs to be installed for command ${pgm}"
+  sudo apt -y install "${pkg}"
+}
+lab(){
+  ip link | grep -C 1 "eth"
+  echo ""
+  ip addr show "enp3s0"
+#  echo "static"
+#  sudo ip link set "enp3s0" down
+#  sudo ip addr add "192.168.1.200" dev "enp3s0"
+#  sudo ip link set "enp3s0" up
+#  echo "dhcp"
+#  sudo ip link set "enp3s0" up
+#  sudo dhclient "enp3s0"
+
+}
+
+setup
+lab "${@}"
+ret="${?}"
+cleanup
+exit "${ret}"
