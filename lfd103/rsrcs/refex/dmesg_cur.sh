ed7e6148a867cbe11393b2613a5120f28fd4d149
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-04-21 06:19:36 +0200

    lfd103 ch 11, 12 testing and debugging

--- lfd103/rsrcs/refex/dmesg_cur.sh
+++ lfd103/rsrcs/refex/dmesg_cur.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+check_empty(){
+  local file_name="${1}"
+  if ! [[ -s "${file_name}" ]]; then
+    echo "${file_name} has some msgs"
+  fi
+}
+
 dmesg_lev(){
   local cur_ver="${1}"
   local level="${2}"
   dmesg -t -l "${level}" > "${cur_ver}.dmesg_${level}"
+  check_empty "${cur_ver}.dmesg_${level}"
 }
 
 release="$(uname -r)"
@@ -15,4 +23,7 @@ dmesg_lev "${release}" "err"
 dmesg_lev "${release}" "warn"
 
 dmesg -t > "${release}.dmesg"
+check_empty "${release}.dmesg"
+
 dmesg -t -k > "${release}.dmesg_kern"
+check_empty "${release}.dmesg_kern"
