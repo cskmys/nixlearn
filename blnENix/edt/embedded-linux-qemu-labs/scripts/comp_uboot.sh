2469a42bb64eb82e72b0b3161c706ebc53475965
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-05-29 02:57:00 +0200

    blnENix automating uboot generation

---
+++ blnENix/edt/embedded-linux-qemu-labs/scripts/comp_uboot.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+CROSS_TOOL="x-tools/arm-training-linux-uclibcgnueabihf/bin/"
+CROSS_TOOL_PATH="${HOME}/${CROSS_TOOL}"
+if ! [[ "${PATH}" == *"${CROSS_TOOL}"* ]]; then
+  PATH="${CROSS_TOOL_PATH}:${PATH}"
+fi
+
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-
+
+UBOOT_SRC_BASE_PATH="/home/csk/edt/nixlearn/blnENix/edt/embedded-linux-qemu-labs/bootloader"
+UBOOT_SRC_PATH="${UBOOT_SRC_BASE_PATH}/u-boot-v2020.04"
+UBOOT_SRC_BCKP_PATH="${UBOOT_SRC_BASE_PATH}/bckp"
+UBOOT_SRC_DATA_PATH="${UBOOT_SRC_BASE_PATH}/data"
+
+if ! [[ -e ${UBOOT_SRC_BCKP_PATH} ]]; then
+  echo "backup is not present, hence no automation"
+  exit 1
+fi
+
+rm -rf "${UBOOT_SRC_PATH}"
+
+cd "${UBOOT_SRC_BCKP_PATH}" || exit 1
+pwd
+
+UBOOT_SRC_BCKP_ARCHIVE="${UBOOT_SRC_BCKP_PATH}/u-boot-v2020.04.tar.gz"
+
+if ! [[ -e "${UBOOT_SRC_BCKP_ARCHIVE}" ]]; then
+  wget https://source.denx.de/u-boot/u-boot/-/archive/v2020.04/u-boot-v2020.04.tar.gz
+fi
+
+cd "${UBOOT_SRC_BASE_PATH}" || exit 1
+pwd
+
+tar xf "${UBOOT_SRC_BCKP_ARCHIVE}"
+
+cd "${UBOOT_SRC_PATH}" || exit 1
+pwd
+
+rm -rf .git*
+
+patch -p1 < "${UBOOT_SRC_DATA_PATH}/vexpress_flags_reset.patch"
+
+make vexpress_ca9x4_defconfig
+
+patch -p1 < "${UBOOT_SRC_BCKP_PATH}/config.patch"
+
+make -j4
--- blnENix/edt/embedded-linux-qemu-labs/scripts/uboot_start.sh
+++ blnENix/edt/embedded-linux-qemu-labs/scripts/uboot_start.sh
@@ -2,6 +2,6 @@
 
 BOOT_LDR_BASE="/home/csk/edt/nixlearn/blnENix/edt/embedded-linux-qemu-labs/bootloader"
 BOOT_LDR_DATA="${BOOT_LDR_BASE}/data"
-BOOT_LDR="${BOOT_LDR_BASE}/u-boot"
+BOOT_LDR="${BOOT_LDR_BASE}/u-boot-v2020.04"
 
 sudo qemu-system-arm -M vexpress-a9 -m 128M -nographic -kernel "${BOOT_LDR}/u-boot" -sd "${BOOT_LDR_DATA}/sd.img" -net tap,script="${BOOT_LDR_DATA}/qemu-myifup.sh" -net nic
